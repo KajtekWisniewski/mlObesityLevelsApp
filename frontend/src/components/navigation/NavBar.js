2dc06b9467de0ffd7b9867df0d4f491934c982fa
KajtekWisniewski <k.wisniewski.603@studms.ug.edu.pl>
2024-05-11 12:59:03 +0200

    inital navbar

--- frontend/src/components/navigation/NavBar.js
+++ frontend/src/components/navigation/NavBar.js
@@ -1,11 +1,18 @@
 'use client';
 import Link from 'next/link';
 import styles from './NavBar.module.scss';
+import Image from 'next/image';
 
 const NavBar = ({}) => {
   return (
     <>
       <nav className={styles.navbar}>
+        <Link className={styles.linkStyle} href={`/`}>
+          <Image src="/logo_transparent.png" width={75} height={75} alt="site logo" />
+        </Link>
+        <Link className={styles.linkStyle} href={`/evaluate/`}>
+          <h2>FIND OUT YOUR OBESITY LEVEL</h2>
+        </Link>
         <Link className={styles.linkStyle} href={`/about/`}>
           <h2>ABOUT</h2>
         </Link>
